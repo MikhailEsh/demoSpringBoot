78452fa5fcf984334ee65bc123d2e884a8491a0d
MikhailEsh <eshtokin.o@yandex.ru>
2018-09-18 21:24:47 +0300

    add dump

--- schema_data.sql
+++ schema_data.sql
@@ -29,3 +29,22 @@ INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (current_timestamp, 'cora.zhang@qq.com', 08613000001123, 'female', 'cora.zhang', 'XuHui District');
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (current_timestamp, 'tom.gao@hotmail.com', 08613000001124, 'female', 'tom.gao', 'HuangPu District');
+
+
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Painting the Tape', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Pump&Dump', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Trash&Cash', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Marking the close   ', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Improperly matched orders     ', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Spoofing', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Layering', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Quote Stuffing', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Inter-trading venue manipulation', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Cross-contract manipulation', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Momentum ignition', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Wash Deals', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Concealing the Ownership', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Pinging', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Advancing the bid', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('N-minute Loss', false);
+INSERT INTO public.algorithms (namealgo, favorite) VALUES ('Off-market deals', false);
